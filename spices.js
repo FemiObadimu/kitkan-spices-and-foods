519c2726ebc4bc8c70c2214fc68c449d5200c682
ficazzosam <66454378+ficazzosam@users.noreply.github.com>
2020-09-25 01:06:59 +0100

    Add files via upload

---
+++ spices.js
@@ -0,0 +1,29 @@
+var btn = document.getElementById('l');
+var btn1 = document.getElementById('i');
+var btn2 = document.getElementById('s');
+
+var buttonClick = function() {
+    if(btn.style.display =='none'){
+        btn.style.display='block';
+    }
+    else{
+        btn.style.display ='none';
+    }
+
+    if(btn1.style.display =='none'){
+        btn1.style.display='block';
+    }
+    else{
+        btn1.style.display ='none';
+    }
+
+    if(btn2.style.display =='none'){
+        btn2.style.display='block';
+    }
+    else{
+        btn2.style.display ='none';
+    }
+
+
+}
+   
